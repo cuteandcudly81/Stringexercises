75218ce157bdf0034430272b660b66fc368d71a8
cuteandcudly81 <muhammed_yusuf@hotmail.com>
2022-03-30 11:40:51 +0200

    String exercises

---
+++ src/main/java/se/lexikon/StringExercises.java
@@ -0,0 +1,18 @@
+package se.lexikon;
+
+public class StringExercises {
+
+    public static void main(String[] args) {
+        /*
+        String name = "Java";
+        System.out.println(name);
+        System.out.println(name.length());
+        */
+        
+    }
+
+
+
+    }
+
+
